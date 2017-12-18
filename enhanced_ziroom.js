32c9579ab47c9c2b8a69ef6285273506fc5478e7
catscarlet <i@catscarlet.com>
2017-12-18 23:06:31 +0800

    删除多余注释，准备发布 v0.1.0

--- enhanced_ziroom.js
+++ enhanced_ziroom.js
@@ -2,7 +2,7 @@
 // @name                Enhanced Ziroom
 // @namespace           https://github.com/catscarlet/enhanced_ziroom
 // @description         Enhanced Ziroom
-// @version             0.0.1
+// @version             0.1.0
 // @author              catscarlet
 // @match               *://*.ziroom.com/z/nl/*.html*
 // @require             https://code.jquery.com/jquery-latest.js
@@ -14,9 +14,7 @@
 window.jQueryLatest = $.noConflict(true);
 
 (function() {
-
     'use strict';
-
     var $ = window.jQueryLatest;
 
     $(function() {
@@ -28,7 +26,6 @@ window.jQueryLatest = $.noConflict(true);
         var houselist = $('#houseList li');
         houselist.each(function(index) {
             var priceDetail = $(this).find('.priceDetail');
-
             var detailUrl = priceDetail.find('.more').find('a').attr('href');
             var detailId = url2id(detailUrl);
             if (detailId) {
@@ -39,7 +36,6 @@ window.jQueryLatest = $.noConflict(true);
     };
 
     function getDetail(detailUrl, detailId) {
-        //console.log(detailUrl);
         $.ajax({
             type: 'GET',
             url: detailUrl,
@@ -55,17 +51,12 @@ window.jQueryLatest = $.noConflict(true);
     }
 
     function checkDetail(msg, detailId) {
-        //console.log(msg);
         var regexp_status_avaliable = new RegExp(/\<a[^\/a].*class=\"btn view\".*\>(.*)\<\/a>/);
         var regexp_status_unavaliable = new RegExp(/\s\<a[^\/a].*class=\"btn view viewGray\".*\>(.*)\<\/a>/);
         var regexp_name = new RegExp(/\<div.*class=\"room_name\".*\>(.|\n)*\<h2\>((.|\n)*)\<\/h2\>/g);
-        //console.log(msg.match(regexp_name));
         var name = domtext2text(msg.match(regexp_name)[0]);
-        //console.log(name);
-
         var status;
         var status_value = false;
-
         var status_avaliable = msg.match(regexp_status_avaliable);
         var status_unavaliable = msg.match(regexp_status_unavaliable);
         if (status_avaliable) {
@@ -80,12 +71,8 @@ window.jQueryLatest = $.noConflict(true);
     }
 
     function drawList(status_value, status, detailId) {
-        //console.log(status);
-        //console.log(status_value);
-        //console.log(detailId);
         var css;
         var thedom = $('#' + detailId);
-
         if (status_value) {
             thedom.text(status);
             thedom.parent().parent().css('background-color', 'lightgreen');
@@ -93,19 +80,15 @@ window.jQueryLatest = $.noConflict(true);
             thedom.text(status);
             thedom.parent().parent().css('background-color', 'grey');
         }
-
     }
 
     function domtext2text(domtext) {
-        //console.log(domtext);
         var rst;
         rst = $.trim($.text($.parseHTML(domtext)));
-        //console.log(rst);
         return rst;
     }
 
     function url2id(url) {
-        //console.log(url)
         var regexp_url2id = new RegExp(/\/(\d+)\.html/);
         var rst = url.match(regexp_url2id);
         if (rst) {
